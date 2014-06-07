521d65039acca535161f66571854a608fc27a48b
kiide <kide.lemark@gmail.com>
2014-06-06 21:40:49 -0300

    First commit

---
+++ HelloWorldNancy/HelloWorldNancy/HelloModule.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Nancy;
+
+namespace HelloWorldNancy
+{
+    public class HelloModule  : NancyModule
+    {
+        public HelloModule ()
+        {
+            Get["/"] = parameters => "Hello world Nancy";
+        }
+    }
+}
